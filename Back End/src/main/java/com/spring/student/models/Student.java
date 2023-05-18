3fcea16c700a3188b6760e4a5f5ea0c41b3e3380
errmili <96416928+errmili@users.noreply.github.com>
2023-05-18 19:52:39 +0200

    add annotation on model student #2

--- Back End/src/main/java/com/spring/student/models/Student.java
+++ Back End/src/main/java/com/spring/student/models/Student.java
@@ -1,24 +1,42 @@
 package com.spring.student.models;
 
 import com.spring.student.models.enums.Gender;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.util.Date;
 
+
+@Entity(name = "student")
 public class Student {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO.IDENTITY)
+    @Column(name = "id")
     private int id;
 
-    private String fullname;
+    @Column(name = "full_name")
+    private String fullName;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender")
     private Gender gender;
 
+    @Column(name = "phone_number")
     private String phone;
 
+    @Column(name = "age")
     private String age;
 
+    @Column(name = "adress")
     private String address;
 
+    @CreationTimestamp
+    @Column(name = "date_created")
     private Date dateCreated;
 
+    @UpdateTimestamp
+    @Column(name = "date_updated")
     private Date dateUpdated;
 }
