b278f8b52a2b6850380f9c01d2b5b5077bcde86b
errmili <96416928+errmili@users.noreply.github.com>
2023-05-18 23:56:20 +0200

    editing files and set configuration #3

--- Back End/src/main/java/com/spring/student/models/Student.java
+++ Back End/src/main/java/com/spring/student/models/Student.java
@@ -1,20 +1,23 @@
 package com.spring.student.models;
-
 import com.spring.student.models.enums.Gender;
 import jakarta.persistence.*;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import java.util.Date;
+import java.util.*;
 
-
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity(name = "student")
 public class Student {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private int id;
+    private Long id;
 
     @Column(name = "full_name")
     private String fullName;
@@ -29,7 +32,7 @@ public class Student {
     @Column(name = "age")
     private String age;
 
-    @Column(name = "adress")
+    @Column(name = "address")
     private String address;
 
     @CreationTimestamp
--- Back End/src/main/java/com/spring/student/models/enums/Gender.java
+++ Back End/src/main/java/com/spring/student/models/enums/Gender.java
@@ -1,5 +1,7 @@
 package com.spring.student.models.enums;
 
+import jakarta.persistence.Enumerated;
+
 public enum Gender {
     MALE,FEMALE
 }
